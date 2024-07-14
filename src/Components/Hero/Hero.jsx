02bf23c9b2117f0cf0bc8d141c7388ff372aea68
estefania-zakovicz <estefaniazakovicz@hotmail.com>
2024-07-14 18:35:27 -0300

    media querys

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-    <div className='hero header'>
+    <div className='hero header' id='inicio'>
       <div className="hero-text">
         <h1>Transforma tu negocio gastronómico con nuestro sistema inteligente: Rápido, Sencillo y Cómodo. Simplifica la gestión de tu restaurante o bar, con la tecnología más avanzada.</h1>
         <button className='btn'>Contactarse <img src={dark_arrow} alt="" /></button>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,55 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
-import logo from '../../assets/mev_logo.png'
-import { Link } from "react-scroll";
+import logo from "../../assets/mev_logo.png";
+import menu_icon from '../../assets/menu-icon.png'
+import { Link, ScrollLink } from "react-scroll";
 
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false);
 
-const [sticky, setSticky] = useState (false)
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 500 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
 
-useEffect(()=>{
-  window.addEventListener('scroll', ()=>{
-    window.scrollY > 500 ? setSticky(true) : setSticky(false);
-  })
-},[]);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = ()=>{
+    mobileMenu ? setMobileMenu(false) : setMobileMenu (true)
+
+  }
 
   return (
     <div className="header">
-    <nav className={`navbar ${sticky? 'dark-nav' : ''}`}>
-        <img src={logo} alt="logo" className="logo"/>
-        <ul>
-          <li><Link to="home" smooth={true} offset={0} duration={500}>Inicio</Link></li>
-          <li><Link to="sobreMev" smooth={true} offset={0} duration={500}>Sobre Mev</Link></li>
-          <li><Link to="Sistema" smooth={true} offset={0} duration={500}>Sistema</Link></li>
-          <li><Link to="Contacto" smooth={true} offset={0} duration={500}>Contacto</Link></li>
+      <nav className={`navbar ${sticky ? "dark-nav" : ""}`}>
+        <img src={logo} alt="logo" className="logo" />
+        <ul className={mobileMenu? '' : 'hide-mobile-menu'}>
+          <li>
+            <Link to="hero" smooth={true} offset={0} duration={500}>
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link to="benefits_1" smooth={true} offset={0} duration={500}>
+              Sobre Mev
+            </Link>
+          </li>
+          <li>
+            <Link to="sistem" smooth={true} offset={-260} duration={500}>
+              Sistema
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} offset={-260} duration={500}>
+              Contacto
+            </Link>
+          </li>
           <li>Iniciar sesión</li>
           <li>Idiomas</li>
         </ul>
+        <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu} />
       </nav>
-      </div>
+    </div>
   );
 };
 
